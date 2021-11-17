bbb2db2ed3df3a067e8427b73130d4b8948cf233
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-11-17 22:29:12 +0530

    [VIGNESH M] Added: ViewPersonCityOrState Method and Added Switch Case on SearchPersonWithCityOrStateMethod

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace AddressBookCollection
             AddressBook book = new AddressBook();
             Dictionary<AddressBook, string> addressBook = new Dictionary<AddressBook, string>();
 
-            Console.WriteLine("1.CreateContact\t2.AddContact\t3.EditWithFirstName\t4.DeletePersonDetails\t5.AddMultipleContact\t6.SearchPersonWithCity");
+            Console.WriteLine("1.CreateContact\t2.AddContact\t3.EditWithFirstName\t4.DeletePersonDetails\t5.AddMultipleContact\t6.ViewPersonByCityOrState");
 
             string key = "y";
             
@@ -42,11 +42,11 @@ namespace AddressBookCollection
 
                     case 5:
                         book.AddMultipleContact();
-                        book.SearchPersonWithCity();
+                        book.SearchPersonWithCityOrState();
                         break;
 
                     case 6:
-                        book.SearchPersonWithCity();
+                        book.ViewPersonCityOrState();
                         break;
 
                     default:
